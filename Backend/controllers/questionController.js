04ad65e09af6448a63b7db717d8de91aa8057a80
Ziad Gad <ziadamrjad@gmail.com>
2024-12-22 06:46:29 +0200

    Adding json files

--- Backend/controllers/questionController.js
+++ Backend/controllers/questionController.js
@@ -1,11 +1,12 @@
-const fs = require("fs");
+const fs = require('fs');
+const AppError = require('../utils/appError');
 
-const data = fs.readFileSync(`${__dirname}/../data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/../data/data.json`, 'utf-8');
 const questions = JSON.parse(data);
 
 exports.getAll = (req, res, next) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: questions.length,
     data: {
       questions,
@@ -16,14 +17,9 @@ exports.getAll = (req, res, next) => {
 exports.getOne = (req, res, next) => {
   const { id } = req.params;
   const question = questions.find((q) => q.id === Number(id));
-  if (!question) {
-    return res.status(404).json({
-      status: "fail",
-      message: "No question with this id",
-    });
-  }
+  if (!question) return next(new AppError('No question with this id', 404));
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       question,
     },
